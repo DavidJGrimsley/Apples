abf338d64ee98aeaec4ca513a2283358a20e7158
David Grimsley <DavidJGrimsley@gmail.com>
2022-10-18 19:20:40 -0400

    Initial Commit

---
+++ Apples/ApplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApplesApp.swift
+//  Apples
+//
+//  Created by DJ on 7/26/22.
+//
+
+import SwiftUI
+
+@main
+struct ApplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
